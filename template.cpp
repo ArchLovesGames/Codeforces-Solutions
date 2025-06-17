f4c291d60b8af04fc6061a29be28a025a985e6c4
ArchLovesGames <archisha.singh@gmail.com>
2025-06-17 19:28:56 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast input-output
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+// Shortcuts
+typedef long long ll; //for long
+typedef vector<int> vi; // for dynamic array
+
+// For multiple test cases
+void solve() {
+    // Sample input-output format
+    // Do something 
+    // Output result
+   
+}
+
+int main() {
+    fast_io;
+}
+
