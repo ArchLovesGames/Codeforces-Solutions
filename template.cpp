07317cd352bec1eb95ece6dec3c84cf813014267
Archisha <archisha.singh@gmail.com>
2025-06-20 11:43:24 +0530

    Edited template
    Edited template
    zsh:1: command not found: wq

--- template.cpp
+++ template.cpp
@@ -1,10 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> //all headers
+using namespace std; //allows one to use commands like cout, cin
+//without the need for using prefix std::
 
-// Fast input-output
+// Fast input-output using cin/cout
 #define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
 
-// Shortcuts
+//macro for a for loop, reduces typing need
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+
+// Shortcuts 
 typedef long long ll; //for long
 typedef vector<int> vi; // for dynamic array
 
@@ -13,10 +17,14 @@ void solve() {
     // Sample input-output format
     // Do something 
     // Output result
-   
 }
+//note: void may be changed to a data type if it is efficient 
+//to return some value instead of printing it
+//eg: checker code with discrete values (Y/N)
 
 int main() {
     fast_io;
+    //input test cases
+    //loop using solve()
+    return 0;
 }
-
